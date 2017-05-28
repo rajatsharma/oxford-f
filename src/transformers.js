038f4c01bb22ddf30b084fa6b11d9eef1b013821
Rajat Sharma <rajat.sharma@abspl.in>
2017-05-29 03:18:55 +0530

    id added to subsense

--- src/transformers.js
+++ src/transformers.js
@@ -1,9 +1,11 @@
 const createWordsArray = (senses, word) => senses.reduce((acc, sense) => {
   let subWords = [];
 
-  sense.subsenses && (subWords = sense.subsenses.map((subsense) => ({ word, meaning: subsense.definitions[0] })));
+  sense.subsenses && (subWords = sense.subsenses.map((subsense) =>
+    ({ id: subsense.id, word, meaning: subsense.definitions[0] })
+  ));
 
-  return [...acc, { id:sense.id, word, meaning: sense.definitions[0], example: sense.examples && sense.examples[0].text }, ...subWords];
+  return [...acc, { id: sense.id, word, meaning: sense.definitions[0], example: sense.examples && sense.examples[0].text }, ...subWords];
 }, []);
 
 module.exports = createWordsArray;
